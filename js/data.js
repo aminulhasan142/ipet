25be1731aa09694257bae5f72cee528f55ac6edc
aminulhasan142 <mahshoukhin142@gmail.com>
2024-11-04 15:37:43 +0600

    before  functionality

---
+++ js/data.js
@@ -0,0 +1,35 @@
+const data = [{
+    id: 01,
+    number: 1,
+    img: '/img/dog.jpg',
+    title: 'Foundation Learning'
+    description1:
+    'Are you ready to turn your love for dogs into a rewarding career? Look no further! Our comprehensive grooming eLearning, called your "Foundation Knowledge," checks what you know, corrects any misunderstandings, and introduces you to the new facts and knowledge you will need when you qualify as a professional dog groomer.',
+    description2:
+    'This stage of our iPET Network Level 3 Diploma in Dog Grooming and Salon Management accounts for around 1/6th of your total qualification time. We aim to get students to complete this before their 1st day of practical education or, at the latest, their last day at the Academy.',
+  
+},
+  {
+    id: 02,
+    number: 2,
+    img: '/img/dog1.jpg',
+    title: 'Practical Learning'
+    description1:
+    'Are you ready to turn your love for dogs into a rewarding career? Look no further! Our comprehensive grooming eLearning, called your "Foundation Knowledge," checks what you know, corrects any misunderstandings, and introduces you to the new facts and knowledge you will need when you qualify as a professional dog groomer.',
+    description2:
+    'This stage of our iPET Network Level 3 Diploma in Dog Grooming and Salon Management accounts for around 1/6th of your total qualification time. We aim to get students to complete this before their 1st day of practical education or, at the latest, their last day at the Academy.',
+  
+  },
+  {
+    id: 03,
+    number: 3,
+    img: '/img/dog2.jpg',
+    title: 'Foundation Learning'
+    description1:
+    'Are you ready to turn your love for dogs into a rewarding career? Look no further! Our comprehensive grooming eLearning, called your "Foundation Knowledge," checks what you know, corrects any misunderstandings, and introduces you to the new facts and knowledge you will need when you qualify as a professional dog groomer.',
+    description2:
+    'This stage of our iPET Network Level 3 Diploma in Dog Grooming and Salon Management accounts for around 1/6th of your total qualification time. We aim to get students to complete this before their 1st day of practical education or, at the latest, their last day at the Academy.',
+  
+},
+  
+]
